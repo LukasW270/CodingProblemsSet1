2457341719e53bcf5d7eac27e7c6267b7f7a3133
LukasW270 <lwitek270@gmail.com>
2024-02-08 14:46:02 -0500

    Updated

--- example1.java
+++ example1.java
@@ -2,6 +2,6 @@ public class Example1
 {
     public static void main (String[] args)
     {
-        System.out.println("\n\n\n\n\n\t\t\t\t TZHS");
+        System.out.println("\n\n\n\n\n\t\t\t\tTZHS");
     }
 }
