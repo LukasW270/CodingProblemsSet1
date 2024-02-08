c592bdf0704b932a625ce9715f97e33c3fc01c29
LukasW270 <lwitek270@gmail.com>
2024-02-08 14:42:42 -0500

    Work done on 2/8/24

--- example2.java
+++ example2.java
@@ -1,3 +1,10 @@
-public class example2 {
-    
+public class Example2
+{
+    public static void main (String[] args)
+    {
+        System.out.println("******************************");
+        System.out.println("*       Sue's Swimwear       *");
+        System.out.println("*  SwimWithSue@swimwear.com  *");
+        System.out.println("******************************");
+    }
 }
--- example3.java
+++ example3.java
@@ -1,3 +1,8 @@
-public class example3 {
-    
+public class Example3
+{
+    public static void main (String[] args)
+    {
+        System.out.println("Lukas Witek\n\nSong: \"This is my fight song!\"\nProverb: \"Some people are so poor that all they have is money.\"");
+
+    }
 }
